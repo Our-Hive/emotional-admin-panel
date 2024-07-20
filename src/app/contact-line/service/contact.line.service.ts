221b917fdeab8f8d6c9eae30c56d5427e985fd25
Nicolas Olmos <yohannicolas2003@gmail.com>
2024-07-20 18:08:16 -0500

    feat[21(contact-line)] add method and dto to create a contact line

--- src/app/contact-line/service/contact.line.service.ts
+++ src/app/contact-line/service/contact.line.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../../environments/environment.development";
 import {GetContactLineResponseDto} from "../dtos/response/get.contact.line.response.dto";
+import {PostContactLineRequestDto} from "../dtos/request/post.contact.line.request.dto";
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,9 @@ export class ContactLineService {
 
   getContactLines() {
     return this.http.get<GetContactLineResponseDto[]>(`${this.apiUrl}`);
+  }
+
+  createContactLine(contactLine: PostContactLineRequestDto) {
+    return this.http.post(`${this.apiUrl}`, contactLine);
   }
 }
