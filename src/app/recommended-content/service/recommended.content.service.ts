b065d52eac5e8489524025dfd07e1affe831604f
Nicolas Olmos <yohannicolas2003@gmail.com>
2024-07-20 19:37:36 -0500

    feat[27(recommended-content)] add dto and a method to create recommended content

--- src/app/recommended-content/service/recommended.content.service.ts
+++ src/app/recommended-content/service/recommended.content.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {environment} from "../../../environments/environment.development";
 import {HttpClient} from "@angular/common/http";
 import {GetRecommendedContentResponseDto} from "../dtos/response/get.recommended.content.response.dto";
+import {PostRecommendedContentRequestDto} from "../dtos/request/post.recommended.content.request.dto";
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,9 @@ export class RecommendedContentService {
 
   getRecommendedContent() {
     return this.httpClient.get<GetRecommendedContentResponseDto[]>(this.apiUrl);
+  }
+
+  createRecommendedContent(recommendedContent: PostRecommendedContentRequestDto) {
+    return this.httpClient.post(this.apiUrl, recommendedContent);
   }
 }
